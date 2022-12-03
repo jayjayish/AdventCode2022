9c445e5b07f55d71c0f338dcfed4671250a6ad2f
Jarjay Chen <jayjayish@gmail.com>
2022-12-02 23:50:22 -0800

    Day 1

---
+++ AdventCode2022-1/Program.cs
@@ -0,0 +1,42 @@
+﻿//Microsoft (R) Visual C# Compiler version 3.4.0-beta4-19562-05 (ff930dec)
+//Copyright (C) Microsoft Corporation. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        using (StreamReader file = new StreamReader("D:\\Documents\\Repos\\AdventCode2022-1\\input.txt"))
+        {
+            int largest = 0;
+            int current = 0;
+            string ln;
+
+            while ((ln = file.ReadLine()) != null)
+            {
+                if (!String.IsNullOrEmpty(ln))
+                {
+                    var val = Int32.Parse(ln);
+                    current += val;
+                }
+                else
+                {
+                    if (current > largest)
+                    {
+                        largest = current;
+                    }
+                    current = 0;
+                }
+            }
+
+            file.Close();
+
+            Console.WriteLine(largest);
+        }
+    }
+}
